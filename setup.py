251861487590231776031d879d8d23e2b47e6576
webf0x@hotmail.com <webf0x@hotmail.com>
2018-01-03 11:43:56 -0500

    Setup project with tox

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name='Menu cycle checker',
+    version='0.1dev',
+    packages=['menu_cycle_checker',],
+    license='The Unlicense',
+    long_description='Check a menu for cycles'
+
+)
