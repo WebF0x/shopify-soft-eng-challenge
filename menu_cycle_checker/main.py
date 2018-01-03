a27c357042b2e409cf45223ae3399d762c01ed7c
webf0x@hotmail.com <webf0x@hotmail.com>
2018-01-03 12:06:07 -0500

    Get a challenge json page

--- menu_cycle_checker/main.py
+++ menu_cycle_checker/main.py
@@ -1,5 +1,17 @@
+import requests
+
+SHOPIFY_CHALLENGE_API_URL = "https://backend-challenge-summer-2018.herokuapp.com/challenges.json"
+
+
+def get_challenge_json(id, page):
+    payload = {"id": id, "page": page}
+    response = requests.get(SHOPIFY_CHALLENGE_API_URL, payload)
+    return response.json()
+
+
 def main():
-    print("Hello world!")
+    challenge_json = get_challenge_json(1, 1)
+    print(challenge_json)
 
 if __name__ == "__main__":
     main()
