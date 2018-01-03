467809444cddaaefde07b4ae44b54ef0a155d47a
webf0x@hotmail.com <webf0x@hotmail.com>
2018-01-03 13:52:36 -0500

    Get menus

--- menu_cycle_checker/main.py
+++ menu_cycle_checker/main.py
@@ -1,21 +1,51 @@
+from math import ceil
+
 import requests
 
-SHOPIFY_CHALLENGE_API_URL = "https://backend-challenge-summer-2018.herokuapp.com/challenges.json"
+SHOPIFY_CHALLENGE_URL = "https://backend-challenge-summer-2018.herokuapp.com/challenges.json"
+SHOPIFY_CHALLENGE_ID = 1
 
 
-def get_challenge_json(id, page):
+def get_json_page(id, page):
     payload = {"id": id, "page": page}
-    response = requests.get(SHOPIFY_CHALLENGE_API_URL, payload)
+    response = requests.get(SHOPIFY_CHALLENGE_URL, payload)
     return response.json()
 
 
-def get_number_of_pages(challenge_json):
-    return challenge_json["pagination"]["total"]
+def get_json_pages():
+    first_page = get_json_page(SHOPIFY_CHALLENGE_ID, 1)
+    number_of_pages = get_number_of_pages(first_page)
+    return [
+        get_json_page(SHOPIFY_CHALLENGE_ID, page_number)
+        for page_number in range(1, number_of_pages + 1)
+    ]
+
+
+def get_number_of_pages(json_page):
+    nb_total_menus = json_page["pagination"]["total"]
+    nb_menus_per_page = json_page["pagination"]["per_page"]
+    return ceil(nb_total_menus / nb_menus_per_page)
+
+
+def get_menus(json_pages):
+    pages_of_menus = [
+        json_page["menus"]
+        for json_page in json_pages
+    ]
+    menus_list = sum(pages_of_menus, [])
+    return {
+        menu["id"]: {
+            key: menu[key] for key in menu if key != "id"
+        }
+        for menu in menus_list
+    }
 
 
 def main():
-    challenge_json = get_challenge_json(1, 1)
-    number_of_pages = get_number_of_pages(challenge_json)
+    json_pages = get_json_pages()
+    menus = get_menus(json_pages)
+    pass
+
 
 if __name__ == "__main__":
     main()
--- test/TestReadChallengeJson.py
+++ test/TestReadChallengeJson.py
@@ -1,13 +1,51 @@
 from menu_cycle_checker.main import get_number_of_pages
+from menu_cycle_checker.main import get_menus
 
 import unittest
 
 
 class TestReadChallengeJson(unittest.TestCase):
     def test_get_number_of_pages(self):
-        challenge_json = {
+        json_page = {
             'pagination': {
-                'total': 4}
+                'total': 15,
+                'current_page': 1,
+                'per_page': 3
+            }
         }
-        number_of_pages = get_number_of_pages(challenge_json)
+        number_of_pages = get_number_of_pages(json_page)
+        self.assertEqual(5, number_of_pages)
+
+    def test_get_number_of_pages_with_non_full_last_page(self):
+        json_page = {
+            'pagination': {
+                'total': 15,
+                'current_page': 1,
+                'per_page': 4
+            }
+        }
+        number_of_pages = get_number_of_pages(json_page)
         self.assertEqual(4, number_of_pages)
+
+    def test_get_menus_from_json_pages(self):
+        json_page_1 = {
+            'menus': [
+                {'id': 1, 'data': 'a', 'child_ids': [2]},
+                {'id': 2, 'data': 'b', 'child_ids': [2, 3]}
+            ]
+        }
+        json_page_2 = {
+            'menus': [
+                {'id': 3, 'data': 'c', 'child_ids': []},
+                {'id': 4, 'data': 'd', 'child_ids': [2]}
+            ]
+        }
+        json_pages = [json_page_1, json_page_2]
+        menus = get_menus(json_pages)
+        expected_menus = {
+            1: {'data': 'a', 'child_ids': [2]},
+            2: {'data': 'b', 'child_ids': [2, 3]},
+            3: {'data': 'c', 'child_ids': []},
+            4: {'data': 'd', 'child_ids': [2]}
+        }
+        self.assertEqual(expected_menus, menus)
